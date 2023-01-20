7d93ccafc604b58e2dc4fe48f5639b6bebc52a97
ImTaeuk <ktuk3862@gmail.com>
2023-01-21 01:42:29 +0900

    Update assignment

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+    if id >= 5000:
+        return  { 'result' : True }
+    else:
+        return { 'result' : False }
+
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    params = request.get_json()
+    return {'name': params['name'] }
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
