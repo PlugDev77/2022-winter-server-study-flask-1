c0cb7b44e509dcd4f9a6687cd77de0a3a6f29586
OruteZ <creeperss85@gmail.com>
2023-01-20 20:46:43 +0900

    serverstudy_week 3

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+    return {"result": bool(id >= 5000)}
+
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    req = request.json()
+    return {'name': req['name']}
+
+
+@app.route('/')
+def test_hello_world():
+    return 'hello, world! server is ok'
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
