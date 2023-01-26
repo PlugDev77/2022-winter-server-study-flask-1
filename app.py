482197715d409cdb5750d5fcd15a9b591dbdbd99
LSH <shms420@gmail.com>
2023-01-26 15:53:23 +0900

    assignment
    get,post

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/id/<int:id>', methods=['GET'])
-def get(id):
-    return {"message": ""}
+@app.route('/id/<int:id>',methods=['GET'])
+def home(id):
+    value = id
+    print(value)
+    if value >= 5000: return "{ ‘result’: true }"
+    else: return "{ ‘result’: false }"
+    
+@app.route('/id',methods = ['POST'])
+def function():
+    data = request.get_json()
+    return data['name']
 
-@app.route('/id', methods=['POST'])
-def post():
-    return {'name': ''}
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
