d20b0f11cb3ae8f02fb66159a20fa0dcaac3e3a1
HyoYongAnn <48045282+HyoYongAnn@users.noreply.github.com>
2023-01-27 23:50:35 +0900

    123

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+    if id >= 5000:
+        return {'result':True}
+    else:
+        return {'result':False}
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    params = request.get_json()
+    name = params['name']
+    return {'name': name}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
+
