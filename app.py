6a607a00816f063f138f686904bc09e4b5c3228d
BoWon <bogod1207@naver.com>
2023-01-30 13:01:02 +0900

    Edit app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+    if(id >=5000):
+        return {"result": "true"}
+    else:
+        return {"result": "false"}
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    param = request.get_json()
+    return jsonify(param)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
