baeebb64a9aa5836289360a7e7301e735b0b13df
Hong <shyboy0113@gmail.com>
2023-02-06 21:56:15 +0900

    2023-02-06
    3주차 과제클리어

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
 def get(id):
-    return {"message": ""}
+
+    if id >=5000:
+     return {'result' : True}
+    else:
+     return {'result': False}
 
 @app.route('/id', methods=['POST'])
 def post():
-    return {'name': ''}
+    get_string = request.json["name"]
+
+    return {'name': get_string}
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
